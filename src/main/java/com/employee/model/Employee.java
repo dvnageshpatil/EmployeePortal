5fcba47f86bd8bdc36659896218f40d0960dfea7
dvnageshpatil <123734366+dvnageshpatil@users.noreply.github.com>
2023-04-03 23:58:01 +0530

    adding mongo db properties (#1)

--- src/main/java/com/employee/model/Employee.java
+++ src/main/java/com/employee/model/Employee.java
@@ -3,11 +3,13 @@ package com.employee.model;
 import jakarta.persistence.Id;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
 @Data
 public class Employee{
     @Id
+    @Field("_id")
     private Integer id;
     private String name;
     private Double salary;
